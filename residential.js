46d4f2132e70c075502f3dc93cba4adc79479e99
Aundre Joseph <josephaundre72@@gmail.com>
2024-11-19 19:12:33 -0800

    name java

---
+++ residential.js
@@ -0,0 +1,13 @@
+const firstuparrow=document.getElementById("first-up-arrow")
+
+const firstdownarrow=document.getElementById("first-down-arrow")
+
+const lastuparrow=document.getElementById("last-up-arrow")
+
+const lastdownarrow=document.getElementById("last-down-arrow")
+
+function sortbyfirstname(order){
+console.log("sort by first name:", order );
+if ( order="async" )
+
+}
